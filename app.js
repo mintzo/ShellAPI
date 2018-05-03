5be0949d0faa52969958cca90ca2e2bee1ff6268
mintzo <mintzmail2@gmail.com>
2018-05-03 16:48:11 +0300

    handeling swagger errors

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-'use strict';
-
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
-module.exports = app; // for testing
+var SwaggerExpress = require('swagger-express-mw')
+var app = require('express')()
+module.exports = app
 
 var config = {
   appRoot: __dirname // required config
-};
+}
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+  if (err) { throw err }
+  swaggerExpress.register(app)
 
-  // install middleware
-  swaggerExpress.register(app);
+  var port = process.env.PORT || 10010
+  app.listen(port)
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
-});
+  app.use(function (err, req, res, next) {
+    // console.error(err.stack)   /// log errors here
+    res.status(500).send('There was a problem handeling the request')
+  })
+})
