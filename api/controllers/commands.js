eb623c7d9912b29e3f4705a1906ceaa880378b7a
mintzo <mintzmail2@gmail.com>
2018-05-07 17:37:27 +0300

    implamented grep command route

--- api/controllers/commands.js
+++ api/controllers/commands.js
@@ -9,7 +9,25 @@ const echoRoute = async (req, res) => {
         res.json({
             output: await shellCommands.echo({ messageToEcho })
         })
-    } catch (shellError) {
-        res.status(400).json({ message: 'the echo message cannot be processed' })
+    } catch (commandError) {
+        res.status(400).json({ message: 'the echo command cannot be processed' })
+    }
+}
+
+module.exports.grepCommand = (req, res) => { grepRoute(req, res) }
+const grepRoute = async (req, res) => {
+    let grepCommandValues = {
+        searchPhrase: req.swagger.params.body.value.searchPhrase,
+        filesToSearch: req.swagger.params.body.value.filesToSearch,
+        filesDirectory: `${__dirname}/../../filesToUseCommandsOn`,
+    }
+    if (grepCommandValues.filesToSearch === undefined) { grepCommandValues.filesToSearch = [] }
+    try {
+        logger.log('info', req.id, `command-grep: ${JSON.stringify(grepCommandValues)}`)
+        res.json({
+            output: await shellCommands.grep(grepCommandValues)
+        })
+    } catch (commandError) {
+        res.status(400).json({ message: commandError })
     }
 }
